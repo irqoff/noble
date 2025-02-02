2528894e5ee031681d9bb6a0920c7f59bb921e03
Aleksandr Usov <irqoff@gmail.com>
2025-02-02 23:43:06 +0200

    tune fish

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,1 +1,17 @@
-starship init fish | source
+if test -d .linuxbrew
+  .linuxbrew/bin/brew shellenv | source
+end
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -x (brew --prefix)"/bin/starship"
+    starship init fish | source
+end
+
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - fish | source
+
+status --is-interactive; and pyenv virtualenv-init - | source
